73c1358d5505335de25a52ee25507479133bdeae
shinsaku.yamamura <shinsaku.yamamura>
2021-09-17 20:45:52 +0900

    :recycle:: 変数名に単位を追加

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
 import os
-import sys
-import json
 import argparse
 
 from reportlab.pdfgen import canvas
@@ -14,7 +12,8 @@ from paper_layout import PaperLayout
 from template_to import TemplateTo
 import metric
 
-FONT_DIR = os.path.join(os.path.dirname(__file__), "fonts/")
+ROOT_DIR = os.path.dirname(__file__)
+FONT_DIR = os.path.join(ROOT_DIR, "fonts/")
 
 FONT_GOTHIC = os.path.join(FONT_DIR, "ipaexg.ttf")
 FONT_MINCHO = os.path.join(FONT_DIR, "ipaexm.ttf")
@@ -58,31 +57,32 @@ class Main():
 
         if self._a4_compatible_mode:
             # A4
-            print_size = A4
-            width_px, height_px = print_size
-            x_offset = (m.px2mm(width_px) - self.layout.width) / 2
-            y_offset = (m.px2mm(height_px) - self.layout.height) / 2
+            print_size_px = A4
+            width_px, height_px = print_size_px
+            x_offset_mm = (m.px2mm(width_px) - self.layout.width) / 2
+            y_offset_mm = (m.px2mm(height_px) - self.layout.height) / 2
         else:
             # Native
-            print_size = (m.mm2px(self.layout.width),
-                          m.mm2px(self.layout.height))
-            width_px, height_px = print_size
-            x_offset = 0
-            y_offset = 0
+            print_size_px = (m.mm2px(self.layout.width),
+                             m.mm2px(self.layout.height))
+            width_px, height_px = print_size_px
+            x_offset_mm = 0
+            y_offset_mm = 0
 
         page = canvas.Canvas(
-            output_path, pagesize=portrait(print_size), bottomup=False)
+            output_path, pagesize=portrait(print_size_px), bottomup=False)
 
-        width = m.px2mm(width_px)
-        height = m.px2mm(height_px)
+        width_mm = m.px2mm(width_px)
+        height_mm = m.px2mm(height_px)
 
         # layout debug
-        self.draw_layout_line(page, m, width, height, x_offset, y_offset)
+        self.draw_layout_line(page, m, width_mm, height_mm,
+                              x_offset_mm, y_offset_mm)
 
         # zip-code
         page.setFont("GOTHIC", 22)
-        page.drawRightString(m.mm2px(x_offset + self.layout.width - 10),
-                             m.mm2px(y_offset + 35),
+        page.drawRightString(m.mm2px(x_offset_mm + self.layout.width - 10),
+                             m.mm2px(y_offset_mm + 35),
                              self.to.zip_code
                              )
 
@@ -93,8 +93,8 @@ class Main():
 
         for x_cursor, line in enumerate(self.to.name):
             for y_cursor, char in enumerate(line):
-                page.drawCentredString(m.mm2px(x_offset + self.layout.width / 2) + (font_size - font_size / 2) - (font_size * x_cursor),
-                                       m.mm2px(y_offset + 65) +
+                page.drawCentredString(m.mm2px(x_offset_mm + self.layout.width / 2) + (font_size - font_size / 2) - (font_size * x_cursor),
+                                       m.mm2px(y_offset_mm + 65) +
                                        font_size * y_cursor,
                                        char
                                        )
@@ -106,8 +106,8 @@ class Main():
 
         for x_cursor, line in enumerate(self.to.address):
             for y_cursor, char in enumerate(line):
-                page.drawCentredString(m.mm2px(x_offset + self.layout.width) - (font_size / lines) - 50 + (font_size - font_size / 2) - (font_size * x_cursor),
-                                       m.mm2px(y_offset + 120) +
+                page.drawCentredString(m.mm2px(x_offset_mm + self.layout.width) - (font_size / lines) - 50 + (font_size - font_size / 2) - (font_size * x_cursor),
+                                       m.mm2px(y_offset_mm + 120) +
                                        font_size * y_cursor,
                                        char
                                        )
@@ -121,8 +121,8 @@ class Main():
             page.setStrokeColorCMYK(0.0, 1.0, 0.8, 0.1)
             page.setLineWidth(m.mm2px(0.5))
 
-            page.rect(m.mm2px(x_offset) + 50 - font_size / 2 - m.mm2px(3),
-                      m.mm2px(y_offset + 160) - font_size - m.mm2px(3),
+            page.rect(m.mm2px(x_offset_mm) + 50 - font_size / 2 - m.mm2px(3),
+                      m.mm2px(y_offset_mm + 160) - font_size - m.mm2px(3),
                       font_size + m.mm2px(6),
                       font_size * lines + m.mm2px(7),
                       1,
@@ -131,8 +131,8 @@ class Main():
 
             page.setFillColorCMYK(0.0, 1.0, 0.8, 0.1)
             for y_cursor, char in enumerate(self.to.contents):
-                page.drawCentredString(m.mm2px(x_offset) + 50,
-                                       m.mm2px(y_offset + 160) +
+                page.drawCentredString(m.mm2px(x_offset_mm) + 50,
+                                       m.mm2px(y_offset_mm + 160) +
                                        font_size * y_cursor,
                                        char
                                        )
@@ -141,32 +141,32 @@ class Main():
 
         print("complite")
 
-    def draw_layout_line(self, page, m, width, height, x_offset, y_offset):
+    def draw_layout_line(self, page, m, width_mm, height_mm, x_offset_mm, y_offset_mm):
         page.setStrokeColorRGB(0.0, 1.0, 1.0)
         page.setLineWidth(m.mm2px(0.5))
 
-        page.line(m.mm2px(x_offset + 0),
+        page.line(m.mm2px(x_offset_mm + 0),
                   m.mm2px(0),
-                  m.mm2px(x_offset + 0),
-                  m.mm2px(height)
+                  m.mm2px(x_offset_mm + 0),
+                  m.mm2px(height_mm)
                   )
 
-        page.line(m.mm2px(x_offset + self.layout.width),
+        page.line(m.mm2px(x_offset_mm + self.layout.width),
                   m.mm2px(0),
-                  m.mm2px(x_offset + self.layout.width),
-                  m.mm2px(height)
+                  m.mm2px(x_offset_mm + self.layout.width),
+                  m.mm2px(height_mm)
                   )
 
         page.line(m.mm2px(0),
-                  m.mm2px(y_offset + 0),
-                  m.mm2px(width),
-                  m.mm2px(y_offset + 0)
+                  m.mm2px(y_offset_mm + 0),
+                  m.mm2px(width_mm),
+                  m.mm2px(y_offset_mm + 0)
                   )
 
         page.line(m.mm2px(0),
-                  m.mm2px(y_offset + self.layout.height),
-                  m.mm2px(width),
-                  m.mm2px(y_offset + self.layout.height)
+                  m.mm2px(y_offset_mm + self.layout.height),
+                  m.mm2px(width_mm),
+                  m.mm2px(y_offset_mm + self.layout.height)
                   )
 
 
